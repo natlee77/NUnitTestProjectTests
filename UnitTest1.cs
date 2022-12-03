5eeae5c4cdd2e7b568fa6c3171d60088c60ba75c
natlee77 <70273134+natlee77@users.noreply.github.com>
2022-12-03 17:39:15 +0100

    cheanged website

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -10,8 +10,7 @@ namespace NUnitTestProjectTests
         private const string _userpassword = "1234567";
          
         private IWebDriver driver;
-        public static string driverpath = @"D:\Program Files\Mozilla\firefox.exe"; //find the web driver path
-
+        
         [SetUp]
         public void Setup()
         {
@@ -19,10 +18,8 @@ namespace NUnitTestProjectTests
             driver = new  OpenQA.Selenium.Edge.EdgeDriver();
 
            
-           driver.Navigate().GoToUrl("https://www.obo.se/");
-        //https://www.obo.se
-            // https://www.zalando.se
-            driver.Manage().Window.Maximize();
+           driver.Navigate().GoToUrl("https://www.inet.se/");         
+           driver.Manage().Window.Maximize();
         }
 
         [Test]
@@ -31,11 +28,23 @@ namespace NUnitTestProjectTests
 
             //Assert.Pass();
             // Ladda ner home page och accept cokkies 
-            var acceptCookies = driver.FindElement(By.Id("wt-cli-accept-all-btn"));            
+            var acceptCookies = driver.FindElement(By.XPath("//button[normalize-space()='Jag förstår']"));            
             acceptCookies.Click();
+
+            //search line 
+            string product = "webbkamera";
+            var search = driver.FindElement(By.XPath("//input[@placeholder='Sök bland 12 039 produkter i 567 kategorier']"));
+            search.SendKeys(product);
+
+            var searchbutton = driver.FindElement(By.XPath("//span[@class='s1vsg01n fa fa-search']"));
+            searchbutton.Click();
+
+            //choose product
+
+
             // click button "Mina Sida " och redirect to Login page
-            var myAccount = driver.FindElement(By.XPath("//span[text()='Mina sidor']"));
-            myAccount.Click();
+            //var myAccount = driver.FindElement(By.XPath("//span[text()='Mina sidor']"));
+            //myAccount.Click();
 
 
 
@@ -45,16 +54,16 @@ namespace NUnitTestProjectTests
             // String content = (String)js.executeScript(loginName);
             // System.out.println(_userlogin);
 
-            var loginName = driver.FindElement(By.CssSelector("input#loginUsername"));
-            loginName.SendKeys(_userlogin);
+            //var loginName = driver.FindElement(By.CssSelector("#loginUsername"));
+            //loginName.SendKeys(_userlogin);
             //(By.Id("loginUsername"));               
             //(By.XPath("//*[@id='loginUsername']"));
 
 
-            var _loginPasssword = driver.FindElement(By.Id("loginPassword"));
-            _loginPasssword.SendKeys(_userpassword);
-            var continueLogin = driver.FindElement(By.XPath("//button[@id='btnLogin']"));
-            continueLogin.Click();
+            //var _loginPasssword = driver.FindElement(By.Id("loginPassword"));
+            //_loginPasssword.SendKeys(_userpassword);
+            //var continueLogin = driver.FindElement(By.XPath("//button[@id='btnLogin']"));
+            //continueLogin.Click();
         }
 
 
@@ -74,7 +83,7 @@ namespace NUnitTestProjectTests
         [TearDown]
         public void TearDown()
         {
-            driver.Quit();
+            //driver.Quit();
         }
     }
 }
