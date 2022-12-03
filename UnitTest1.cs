4c8d5194f74cfb2428780fa8a5eb17730eaa587e
natlee77 <70273134+natlee77@users.noreply.github.com>
2022-12-03 15:12:46 +0100

    addad

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 
 namespace NUnitTestProjectTests
 {
@@ -8,22 +9,40 @@ namespace NUnitTestProjectTests
         private const string _userlogin = "natlisjo@gmail.com";
         private const string _userpassword = "1234567";
 
+        //static FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"C:\Users\abcd\Downloads\geckodriver-v0.13.0-win64", "geckodriver.exe");
+        //service.FirefoxBinaryPath= @"D:\Program Files\Mozilla\firefox.exe";
+        //service.Port = 64444;        
+        //service.HideCommandPromptWindow = true;
+        //service.SuppressInitialDiagnosticInformation = true;
+
+
+        IWebDriver Driver = new FirefoxDriver();
 
         private IWebDriver driver;
-       
-       
+        public static string driverpath = @"D:\Program Files\Mozilla\firefox.exe"; //find the web driver path
+
         [SetUp]
         public void Setup()
         {
-            driver = new OpenQA.Selenium.Edge.EdgeDriver();
-            driver.Navigate().GoToUrl("https://www.obo.se/");
+
+            driver = new FirefoxDriver(driverpath);
+                         //OpenQA.Selenium.Edge.EdgeDriver();
+
+           
+           driver.Navigate().GoToUrl("https://www.obo.se/");
+        //https://www.obo.se
+            // https://www.zalando.se
             driver.Manage().Window.Maximize();
         }
 
         [Test]
         public void LoginTest()
-        {            
+        {
+
+            ////Assert.Pass();
             var acceptCookies = driver.FindElement(By.Id("wt-cli-accept-all-btn"));
+            ////(By.XPath("//button[@id='uc-btn-deny-banner']"));
+            ////
             acceptCookies.Click();
 
             var signIn = driver.FindElement(By.XPath("//span[text()='Mina sidor']"));
@@ -32,19 +51,21 @@ namespace NUnitTestProjectTests
 
 
 
-            var loginName = "return window.getComputedStyle(document.querySelector('#loginUsername'),':before') ";
-            JavascriptExecutor js = (JavascriptExecutor)driver;
-             String content = (String)js.executeScript(loginName);
-             System.out.println(_userlogin);
-            //var loginName = driver.FindElement (By.CssSelector("input#loginUsername")); 
+            //var loginName = "return window.getComputedStyle(document.querySelector('#loginUsername'),':before') ";
+            //JavascriptExecutor js = (JavascriptExecutor)driver;
+            // String content = (String)js.executeScript(loginName);
+            // System.out.println(_userlogin);
+
+            var loginName = driver.FindElement(By.CssSelector("input#loginUsername"));
+            loginName.SendKeys(_userlogin);
             //(By.Id("loginUsername"));               
             //(By.XPath("//*[@id='loginUsername']"));
-             //loginName.SendKeys(_userlogin);
+             
 
             //var _loginPasssword = driver.FindElement(By.Id("loginPassword"));
             // _loginPasssword..SendKeys(_userpassword);
-            var  continueLogin = driver.FindElement(By.XPath("//button[@id='btnLogin']"));
-            continueLogin.Click();
+            //var  continueLogin = driver.FindElement(By.XPath("//button[@id='btnLogin']"));
+            //continueLogin.Click();
         }
 
 
@@ -64,7 +85,7 @@ namespace NUnitTestProjectTests
         [TearDown]
         public void TearDown()
         {
-
+            driver.Quit();
         }
     }
 }
